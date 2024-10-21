673215ec7114f925509e60687fd2023bdeb1cbdc
Sergii Symonenko <simonenkos@gmail.com>
2024-10-21 15:04:16 +0200

    Init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import {ReactElement} from "react";
+
+const App = (): ReactElement => {
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
+};
+
+export default App
