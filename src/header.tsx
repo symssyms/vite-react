d79738939bac03cf7310e15e4fc0b37d270dacdc
Sergii Symonenko <simonenkos@gmail.com>
2024-10-23 11:19:17 +0200

    updated vite.config.ts

--- src/header.tsx
+++ src/header.tsx
@@ -6,7 +6,9 @@ import Navigation     from './navigation.tsx';
 const Header = (): ReactElement => {
     return (
         <Box sx={styles} className={'header'}>
-            <img src={'/logo.svg'} width={154} height={20}/>
+            <img
+                src={'https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg'}
+                width={154} height={20}/>
             <Navigation items={navItems}/>
         </Box>
     );
